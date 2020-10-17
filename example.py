8ab26e5384c9bf71c28d21b4c197e99c597a8a35
b-d-wilson <68611524+b-d-wilson@users.noreply.github.com>
2020-10-17 10:17:32 -0400

    Implement naive join_string and time it

---
+++ example.py
@@ -0,0 +1,14 @@
+from time import perf_counter as timer
+
+def join_words(words):
+    """Joins words using string concatenation"""
+    sentence = ""
+    for word in words:
+        sentence += word
+    return sentence
+
+test_words = ['hehe', 'haha', 'hoho', 'hawhaw', 'ehehe', 'muahahahahhaha'] * 100000
+start = timer()
+joined = join_words(test_words)
+stop = timer()
+print(f'Runtime of join_words was {stop - start:0.4f} seconds')
