ccd0ba1cb2fe50b402afdad5e900a59ea31f8f76
b-d-wilson <68611524+b-d-wilson@users.noreply.github.com>
2020-10-17 11:38:39 -0400

    Implement StringBuilder and compare to str.join

--- example.py
+++ example.py
@@ -1,7 +1,9 @@
 from time import perf_counter as timer
 from typing import List
 
-def join_words(words: List[str]) -> str:
+from stringbuilder import StringBuilder
+
+def join_words_cat(words: List[str]) -> str:
     """Joins words using string concatenation"""
     sentence = ""
     for word in words:
@@ -9,8 +11,31 @@ def join_words(words: List[str]) -> str:
     return sentence
 
 
-test_words = ['hehe', 'haha', 'hoho', 'hawhaw', 'ehehe', 'muahahahahhaha'] * 100000
+def join_words_builder(words: List[str]) -> str:
+    """Joins words using a StringBuilder"""
+    builder = StringBuilder()
+    for word in words:
+        builder.append(word)
+    return builder.to_string()
+
+
+def join_words_join(words: List[str]) -> str:
+    """Joins words using str.join"""
+    return ''.join(words)
+
+test_words = ['hehe', 'haha', 'hoho', 'hawhaw', 'ehehe', 'muahahahahhaha'] * 10000000
+
 start = timer()
-joined = join_words(test_words)
+joined = join_words_cat(test_words)
 stop = timer()
-print(f'Runtime of join_words was {stop - start:0.4f} seconds')
+print(f'Runtime of join_words_cat was {stop - start:0.4f} seconds')
+
+start = timer()
+joined = join_words_builder(test_words)
+stop = timer()
+print(f'Runtime of join_words_builder was {stop - start:0.4f} seconds')
+
+start = timer()
+joined = join_words_join(test_words)
+stop = timer()
+print(f'Runtime of join_words_join was {stop - start:0.4f} seconds')
---
+++ stringbuilder.py
@@ -0,0 +1,14 @@
+"""Implementation of a StringBuilder"""
+
+class StringBuilder():
+    """Builds a string"""
+
+    def __init__(self, delimiter: str = '') -> None:
+        self.delimiter = delimiter
+        self.charlist = []
+
+    def append(self, word: str) -> None:
+        self.charlist += list(word)
+
+    def to_string(self) -> str:
+        self.delimiter.join(self.charlist)
